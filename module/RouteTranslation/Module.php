a1dad6fdaf0d88046ef16db7427c720286a7fd4c
Sven Friedemann <sven@erstellbar.de>
2015-04-29 15:30:37 +0200

    Add route translation module to make segement routes translatable

---
+++ module/RouteTranslation/Module.php
@@ -0,0 +1,40 @@
+<?php
+namespace RouteTranslation;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $eventManager->attach(MvcEvent::EVENT_ROUTE, array($this, 'onPreRoute'), 100);
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function onPreRoute($e)
+    {
+        $app            = $e->getTarget();
+        $serviceManager = $app->getServiceManager();
+        $translator     = $serviceManager->get('translator');
+        $serviceManager->get('router')->setTranslator($translator);
+    }
+}
