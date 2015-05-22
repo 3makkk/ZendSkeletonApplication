24e445f25cc6ae387c033df02e6e731eb30839fb
Sven Friedemann <sven.friedemann@gmx.de>
2015-05-22 09:51:23 +0200

    outsourced MailLog as composer package

--- module/MailLog/config/module.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'service_manager' => array(
-        'factories' => array(
-            'MailLog\Logger' => 'MailLog\Log\Service\MailLogLoggerFactory'
-        )
-    )
-);
